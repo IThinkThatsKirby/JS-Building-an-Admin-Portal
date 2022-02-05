5c9dc53516f46a699b68a17a5a1a2af64c77f148
Kirby Ellingson <hkellingson@gmail.com>
2022-02-05 15:37:33 -0600

    it does do update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function main(){
 
-// Your Code Here
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.querySelector('#root');
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let input = document.createElement('input')
+    input.value = book.quantity
+
+    let but = document.createElement('button')
+    but.textContent = 'Save'
+
+    but.addEventListener('click',()=>{fetch('http://localhost:3001/updateBook',{
+        method: "PATCH",
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify ({
+            id: book.id,
+            quantity: input.value
+        }),
+        
+    })
+    })
+
+    root.append(li,input,but)
+}
+
+main();
